c9984870e575a822ab353c9bbd6e0f8b7760940c
Siddhant779 <panditsiddhant.sp@utexas.edu>
2024-04-17 16:33:40 -0500

    basic format for SD Card
    Signed-off-by: Siddhant779 <panditsiddhant.sp@utexas.edu>

---
+++ sw/src/SD.h
@@ -0,0 +1,21 @@
+#ifndef _SDH_
+#define _SDH_
+#include <stdint.h>
+
+//init function for starting it up 
+extern void SD_initSDCard(void);
+
+//for opening the file based on the filename 
+extern void SD_openFile(char* filename);
+
+//for closing the file based on the filename
+extern void SD_closeFile(char *filename)
+
+//for reading the file - pass in what buffer you want and how many bytes for reading 
+extern void SD_read(uint8_t* buffer, uint32_t numBytes);
+
+//for reading the next Char 
+extern char SD_readNextChar();
+
+
+#endif
