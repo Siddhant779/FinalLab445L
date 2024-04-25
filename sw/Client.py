70c4c1ba14a3c1bf9123812d8468f115af316d81
Shyoon2 <hsings022@gmail.com>
2024-04-24 23:08:36 -0500

    Merge conflict changes

--- sw/Client.py
+++ sw/Client.py
@@ -1,29 +1,71 @@
 import socket
+import threading
 
-SERVER_HOST = "127.0.0.1"
+SERVER_HOST = "172.20.10.10"
 SERVER_PORT = 8080
 
-def main():
-    # Create the TCP connection
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+class Client():
+    def __init__(self):
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            client_socket.connect((SERVER_HOST, SERVER_PORT))
+            print(f"[+] Connected to the server at {SERVER_HOST}:{SERVER_PORT}")
+            self.socket = client_socket
+        except Exception as e:
+            print(f"[-] Error while connecting to Client: {e}")
+            client_socket.close()
+        
+    def start(self):
+        input_thread = threading.Thread(target=self.input_func)
+        output_thread = threading.Thread(target=self.output_func)
+        input_thread.start()
+        output_thread.start()
+
     
-    try:
-        client_socket.connect((SERVER_HOST, SERVER_PORT))
-        print(f"[+] Connected to the server at {SERVER_HOST}:{SERVER_PORT}")
-        
-        msg = "Hello world!"
-        client_socket.sendall(msg.encode())
-        
-        print(f"[+] SEND: {msg}")
-        
-        response = client_socket.recv(1024).decode()
-        
-        print(f"[+] RECV: {response}")
-    except Exception as e:
-        print(f"[-] Error: {e}")
-    finally:
-        print(f"[-] Disconnecting from server")
-        client_socket.close()
+    def input_func(self):
+        while True:
+            try:
+                recved = self.socket.recv(1024).decode()
+                print(f"\n[+] RECV: {recved}")
+            except Exception as e:
+                print(f"[-] Error when receiving a message: {e}")
+                self.socket.close()
+                break                
+    
+    def output_func(self):
+        while True:
+            try:
+                msg = input("Enter a message: ")
+                self.socket.sendall(msg.encode())
+            except Exception as e:
+                print(f"[-] Error when sending a message: {e}")
+                self.socket.close()
+                break
+    
 
 if __name__ == "__main__":
-    main()
+    c = Client()
+    c.start()
+    
+####### Test Code #######
+# def main():
+#     # Create the TCP connection
+#     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+#     try:
+#         client_socket.connect((SERVER_HOST, SERVER_PORT))
+#         print(f"[+] Connected to the server at {SERVER_HOST}:{SERVER_PORT}")
+        
+#         msg = "Hello world!"
+#         client_socket.sendall(msg.encode())
+        
+#         print(f"[+] SEND: {msg}")
+        
+#         response = client_socket.recv(1024).decode()
+        
+#         print(f"[+] RECV: {response}")
+#     except Exception as e:
+#         print(f"[-] Error: {e}")
+#     finally:
+#         print(f"[-] Disconnecting from server")
+#         client_socket.close()
--- sw/Server.py
+++ sw/Server.py
@@ -1,38 +1,71 @@
 import socket
 import threading
 
-HOST = "127.0.0.1"
+HOST = "172.20.10.10"
 PORT = 8080
 
-def client_handler(socket):
-    while True:
-        try:
-            data = socket.recv(1024)
-            
-            if not data:
+client_dict = {}
+
+class ClientHandler():
+    def __init__(self, socket, num, addr, port):
+        self.socket = socket
+        self.num = num
+        self.addr = addr
+        self.port = port
+    
+    def handle_input(self):
+        while True:
+            try:
+                data = self.socket.recv(1024).decode()
+                if not data:
+                    break
+                
+                print(f"[+] Received data from client {self.num}: {data}")
+                
+                self.send_msg(data)
+            except Exception as e:
+                print(f"[!] Error occurred while handling input from client {self.num}: {e}")
                 break
-            
-            print(f"[!] Received {data.decode()} from client")
-            
-            # Echo for now
-            socket.send(data)
-        except Exception as e:
-            print(f"Error handling client: {e}")
-            break
-    socket.close()
+        self.socket.close()
+        del client_dict[self.num]
+        
+    
+    def send_msg(self, msg):
+        # XOR operation
+        if len(client_dict) > 1:
+            for k in client_dict.keys():
+                if k != self.num:
+                    client_socket = client_dict[k].get_socket()
+                    client_socket.send(msg.encode())    
+        else:
+            print(f"[!] Sending message failed, only one client in client_dict")
+        
+    def get_socket(self):
+        return self.socket
+    
+    def get_num(self):
+        return self.num
+    
+    def get_info(self):
+        return f"{self.addr}:{self.port}"
 
 def main():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((HOST, PORT))
     server_socket.listen(2)                                                 # Only allow 2 connections
     print(f"[*] Listening on {HOST}:{PORT}")
+    unique_num = 0
     
     try:
         while True:
             client_socket, client_addr = server_socket.accept()             # Blocking until client connects
             print(f"[+] Accepted connection from {client_addr[0]}:{client_addr[1]}")
             
-            client_thread = threading.Thread(target=client_handler, args=(client_socket,))
+            handler = ClientHandler(client_socket, unique_num, client_addr[0], client_addr[1])
+            client_dict[unique_num] = handler
+            unique_num += 1
+            
+            client_thread = threading.Thread(target=handler.handle_input)
             client_thread.start()
     except KeyboardInterrupt:
         print(f"[*] Closing server...")
